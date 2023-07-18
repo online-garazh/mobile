74a93d9f01ecf1deea9683925b36ea3dd2a4622e
oleksandr.kupriienko <oleksandr.kupriienko@softconstruct.com>
2023-07-18 11:38:33 +0300

    starter

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/features/login/pages/login.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
