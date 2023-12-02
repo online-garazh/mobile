a53aa6fc791888a1aa95238d3c34e3ddf6c2e853
oleksandr.kupriienko <4kuper@gmail.com>
2023-12-02 17:06:09 +0200

    refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:mobile/app.dart';
 
-void main() async {
-  await runZonedGuarded(
-    () async => runApp(const App()),
+void main() {
+  runZonedGuarded(
+    () {
+      WidgetsFlutterBinding.ensureInitialized();
+      runApp(const App());
+    },
     (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
   );
-  runApp(const App());
 }
