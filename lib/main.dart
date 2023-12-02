55425816fdd914a0188172977afcdd324ee8843c
oleksandr.kupriienko <4kuper@gmail.com>
2023-12-02 16:23:07 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
 import 'dart:async';
 import 'dart:developer';
 
-
 import 'package:flutter/material.dart';
 import 'package:mobile/app.dart';
 
-void main() {
-  runZonedGuarded(
-    () => runApp(const App()),
-    (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
-  );
 void main() async {
   await runZonedGuarded(
     () async => runApp(const App()),
