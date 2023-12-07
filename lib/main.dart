54e99e29c1e0c08b1964c5cd07a6b3a81d63b12e
joker960555 <joker9605@gmail.com>
2023-12-07 22:51:45 +0200

    Firebase integrated. Android Notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'dart:async';
 import 'dart:developer';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:mobile/api/firebase_api.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile/app.dart';
 
-void main() {
+Future<void> main() async {
   runZonedGuarded(
-    () {
+    () async {
       WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
+      await FirebaseApi().initNotification();
+      FirebaseFirestore.instance;
+
       runApp(const App());
     },
     (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
