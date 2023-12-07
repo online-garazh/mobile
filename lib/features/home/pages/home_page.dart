c79376cbee5c18f225859cb3dd35842d6e8be043
oleksandr.kupriienko <4kuper@gmail.com>
2023-12-07 15:51:24 +0200

    add navigator

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/components/custom_app_bar.dart';
-import 'package:mobile/components/custom_icon_button.dart';
 import 'package:mobile/components/custom_text_button.dart';
 import 'package:mobile/components/spacers.dart';
 import 'package:mobile/features/login/pages/login.dart';
-import 'package:mobile/features/profile/pages/profile_page.dart';
-import 'package:mobile/theme/custom_styles.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -13,21 +9,6 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(
-        title: const Text('HomePage'),
-        actions: [
-          CustomIconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const ProfilePage()),
-              );
-            },
-            icon: Icons.person,
-            color: AppPalette.black,
-          )
-        ],
-      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.stretch,
